3ab66a2c0e0e0bcfb5e47854561049f62cb0e30d
iosアカデミア <yutamac2512@icloud.com>
2025-04-20 17:39:34 +0900

    SettingViewリファクタリング

--- MyShoppingListApp/MyShoppingListApp/Views/Setting/SettingView.swift
+++ MyShoppingListApp/MyShoppingListApp/Views/Setting/SettingView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import RealmSwift
 import StoreKit
+
+class Nickname: Object, Identifiable {
+    @Persisted(primaryKey: true) var id: String = UUID().uuidString
+    @Persisted var nickname: String?
+}
 
 struct SettingView: View {
     
     @AppStorage(wrappedValue: 0, "appearanceMode") var appearanceMode
     @Environment(\.requestReview) var requestReview
+    @State private var nickname = ""
     
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
     let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
@@ -19,6 +26,24 @@ struct SettingView: View {
     var body: some View {
         NavigationStack {
             List {
+                Section("プロフィール") {
+                    VStack {
+                        HStack {
+                            Image(systemName: "person")
+                                .font(.system(size: 30))
+                            TextField("ニックネーム", text: $nickname)
+                                .onChange(of: nickname) { newValue in
+                                    saveNickname(newValue)
+                                }
+                                .font(.system(size: 20))
+                        }
+                        Text("ニックネームの設定は任意ですが、リストを共有する場合はニックネームを設定してください")
+                            .font(.caption)
+                    }
+                    .frame(height: 60) // 縦幅を80ポイントに設定
+                        
+
+                }
                 Section("設定") {
                     HStack {
                         Text("外観モード")
@@ -30,7 +55,8 @@ struct SettingView: View {
                             Text("ダーク")
                                 .tag(2)
                         }
-                        .pickerStyle(.menu)
+                        .pickerStyle(.automatic)
+                        .frame(height: 20) // 縦幅を80ポイントに設定
                     }
                     NavigationLink(destination: ShareView()) {
                         Text("共有設定")
@@ -55,9 +81,37 @@ struct SettingView: View {
                         requestReview()
                     }
                     .foregroundColor(Color.primary)
+                    Text("アプリを共有")
+//                    Link("アプリを共有", destination: URL(string: "")!)
                 }
             }
             .listStyle(.grouped)
+            .onAppear {
+                loadNickname()
+            }
+        }
+    }
+    
+    private func saveNickname(_ newNickname: String) {
+        let realm = try! Realm()
+        if let existingNickname = realm.objects(Nickname.self).first {
+            try! realm.write {
+                existingNickname.nickname = newNickname
+            }
+        } else {
+            let nicknameObject = Nickname()
+            nicknameObject.nickname = newNickname
+            try! realm.write {
+                realm.add(nicknameObject)
+            }
+        }
+    }
+    
+    // Realmからニックネームをロード
+    private func loadNickname() {
+        let realm = try! Realm()
+        if let existingNickname = realm.objects(Nickname.self).first {
+            nickname = existingNickname.nickname ?? ""
         }
     }
 }
