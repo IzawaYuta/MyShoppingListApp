38eaa76421dab89b3a159bc2fe253ee6fbea23b2
yutaizawa <yutamac2512@icloud.com>
2025-09-23 18:45:36 +0900

    Modelファイル作成

--- MyShoppingListApp/MyShoppingListApp/Views/CategoryListView.swift
+++ MyShoppingListApp/MyShoppingListApp/Views/CategoryListView.swift
@@ -10,43 +10,6 @@ import RealmSwift
 import FirebaseAnalytics
 import FirebaseAnalytics
 
-// MARK: Item
-class Item: Object, Identifiable {
-    @Persisted var id = UUID()
-    @Persisted var name: String = ""
-    @Persisted var isChecked: Bool = false
-    
-    convenience init(name: String) {
-        self.init()
-        self.name = name
-    }
-}
-
-// MARK: CategoryListModel
-class CategoryListModel: Object, Identifiable {
-    @Persisted(primaryKey: true) var id: String = UUID().uuidString
-    @Persisted var name: String = ""
-    @Persisted var items = RealmSwift.List<Item>()
-    @Persisted var sortIndex: Int // 並び順を保持
-    @Persisted var regularItems = RealmSwift.List<RegularItem>() // 定期品リスト
-    @Persisted var isOn: Bool = false
-    
-    convenience init(name: String, items: [String], regularItems : [String]) {
-        self.init()
-        self.name = name
-        self.items.append(objectsIn: items.map { Item(name: $0) }) // 変換して追加
-        self.items.append(objectsIn: items.map { Item(name: $0) }) // 変換して追加
-    }
-    
-    var itemCount: Int {
-        return items.count
-    }
-    var uncheckedItemCount: Int {
-        return items.filter { !$0.isChecked }.count
-    }
-}
-
-
 // MARK: CategoryListView
 struct CategoryListView: View {
     
